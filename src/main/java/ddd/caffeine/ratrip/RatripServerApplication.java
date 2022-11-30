ea5e231c77479a7e2f4c16463ee884c11ecedcd6
JIWEON-JEONG <dlektl6@naver.com>
2022-11-30 15:00:14 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/ddd/caffeine/ratrip/RatripServerApplication.java
@@ -0,0 +1,13 @@
+package ddd.caffeine.ratrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatripServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatripServerApplication.class, args);
+	}
+
+}

e2d45a8de07648dafacafe9b594fb4c45b5c3f88
JIWEON-JEONG <dlektl6@naver.com>
2022-12-04 12:19:48 +0900

    feat : API KEY ignore

--- src/main/java/ddd/caffeine/ratrip/service/RecommendPlaceService.java
+++ src/main/java/ddd/caffeine/ratrip/service/RecommendPlaceService.java
@@ -1,13 +1,14 @@
 package ddd.caffeine.ratrip.service;
 
+import org.springframework.stereotype.Service;
+
 import ddd.caffeine.ratrip.RecommendPlaceUseCase;
 import ddd.caffeine.ratrip.controller.dto.RecommendResponseDto;
-import org.springframework.stereotype.Service;
 
 @Service
 public class RecommendPlaceService implements RecommendPlaceUseCase {
-    @Override
-    public RecommendResponseDto recommendPlace(String region, String placeCategory) {
+	@Override
+	public RecommendResponseDto recommendPlace(String region, String placeCategory) {
 
-    }
+	}
 }

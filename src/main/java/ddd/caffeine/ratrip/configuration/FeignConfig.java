f9742cda6334bf98efc5be92c204f9bcab94dbee
JIWEON-JEONG <dlektl6@naver.com>
2022-12-04 14:13:56 +0900

    feat : Retry 정책 생성

--- src/main/java/ddd/caffeine/ratrip/configuration/FeignConfig.java
+++ src/main/java/ddd/caffeine/ratrip/configuration/FeignConfig.java
@@ -4,16 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import feign.Logger;
+import feign.Retryer;
 
 @Configuration
 public class FeignConfig {
 
 	/**
-	 * FULL : request, response의 headers, body 그리고 metadata를 모두 로깅.
+	 * FULL: request, response의 headers, body 그리고 metaData를 모두 로깅.
 	 */
 	@Bean
 	Logger.Level feignLoggerLevel() {
 		return Logger.Level.FULL;
+	}
+
+	/**
+	 * Retry 정책
+	 * - 1초를 시작으로 최대 2초까지 Retry.
+	 * - 최대 3번 Retry.
+	 */
+	@Bean
+	public Retryer retryer() {
+		return new Retryer.Default(1000, 2000, 3);
 	}
 
 	// @Bean

fc161a8504a6edb85e3f029573ed242b422cf7ab
jihwan <sgh002400@naver.com>
2022-12-09 22:02:01 +0900

    fix(TokenResponseDto): 응답 Dto에 Getter 추가

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/response/TokenResponseDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/response/TokenResponseDto.java
@@ -2,8 +2,10 @@ package ddd.caffeine.ratrip.module.auth.presentation.dto.response;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class TokenResponseDto {
 	private String accessToken;
 	private String refreshToken;

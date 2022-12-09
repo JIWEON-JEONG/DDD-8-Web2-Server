10e86c98fe9cf226e6c3ff51d090e15a7a08febd
jihwan <sgh002400@naver.com>
2022-12-09 21:33:55 +0900

    fix(AuthController): 문제 생기는 uri 임시 삭제

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/AuthController.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/AuthController.java
@@ -3,7 +3,6 @@ package ddd.caffeine.ratrip.module.auth.presentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ddd.caffeine.ratrip.module.auth.application.AuthService;
@@ -19,7 +18,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping(value = "/v1")
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthServiceProvider authServiceProvider;

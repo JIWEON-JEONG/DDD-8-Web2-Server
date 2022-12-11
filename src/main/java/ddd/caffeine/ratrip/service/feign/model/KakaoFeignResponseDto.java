067032ee8a00110c092a46501c67dd0068fdf851
JIWEON-JEONG <dlektl6@naver.com>
2022-12-11 19:51:02 +0900

    test : 페이징 사이즈 유효성 검사 테스트

--- src/main/java/ddd/caffeine/ratrip/service/feign/model/KakaoFeignResponseDto.java
+++ src/main/java/ddd/caffeine/ratrip/service/feign/model/KakaoFeignResponseDto.java
@@ -5,6 +5,9 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * Todo: 디자인 분들과 프론트 분들 필요한 정보 받아서 최적화 할 예정
+ */
 @Getter
 @NoArgsConstructor
 public class KakaoFeignResponseDto {
---
+++ src/test/java/ddd/caffeine/ratrip/service/RecommendPlaceServiceTest.java
@@ -0,0 +1,43 @@
+package ddd.caffeine.ratrip.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+
+import ddd.caffeine.ratrip.exception.domain.KakaoFeignException;
+import ddd.caffeine.ratrip.service.feign.KakaoFeignClient;
+
+/**
+ * RecommendPlaceService 의 단위테스트 구현
+ */
+class RecommendPlaceServiceTest {
+
+	RecommendPlaceService recommendPlaceService;
+
+	@Mock
+	KakaoFeignClient kakaoFeignClient;
+
+	@BeforeEach
+	void init() {
+		recommendPlaceService = new RecommendPlaceService(kakaoFeignClient);
+	}
+
+	@ParameterizedTest
+	@DisplayName("페이징 사이즈 유효성 검사 테스트")
+	@ValueSource(ints = {-1, 0, 46, 1000000})
+	void validatePageSizeTest(int pageSize) {
+		//given
+		String region = "서울 이태원";
+		String keyword = "볼거리";
+
+		//then
+		assertThatThrownBy(() ->
+			recommendPlaceService.recommendPlace(region, keyword, pageSize))
+			.isInstanceOf(KakaoFeignException.class)
+			.hasMessage("Page 는 1 이상 45 이하 여야 합니다.");
+	}
+}

3d62b754409ba311e20c3f9d3d720e45162f8568
JIWEON-JEONG <dlektl6@naver.com>
2022-12-12 18:50:11 +0900

    refactor : validator 클래스 분리

--- src/main/java/ddd/caffeine/ratrip/module/recommend/service/RecommendPlaceService.java
+++ src/main/java/ddd/caffeine/ratrip/module/recommend/service/RecommendPlaceService.java
@@ -3,8 +3,6 @@ package ddd.caffeine.ratrip.module.recommend.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import ddd.caffeine.ratrip.common.exception.ExceptionInformation;
-import ddd.caffeine.ratrip.common.exception.domain.KakaoFeignException;
 import ddd.caffeine.ratrip.module.recommend.domain.KakaoFeignModel;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +16,11 @@ public class RecommendPlaceService implements RecommendPlaceUseCase {
 	private String KAKAO_API_KEY;
 	private final KakaoFeignClient kakaoFeignClient;
 
+	private final RecommendPlaceValidator recommendPlaceValidator;
+
 	@Override
 	public KakaoFeignModel recommendPlaces(String region, String keyword, int page) {
-		validatePageSize(page);
+		recommendPlaceValidator.validatePageSize(page);
 		return readPlaces(region, keyword, page);
 	}
 
@@ -28,13 +28,5 @@ public class RecommendPlaceService implements RecommendPlaceUseCase {
 		final String KAKAO_REQUEST_HEADER = "KakaoAK " + KAKAO_API_KEY;
 		String query = region + " " + keyword;
 		return kakaoFeignClient.readPlacesByKeywordAndCategory(KAKAO_REQUEST_HEADER, query, page);
-	}
-
-	private void validatePageSize(int page) {
-		final int MIN_PAGE = 1;
-		final int MAX_PAGE = 45;
-		if (page < MIN_PAGE || page > MAX_PAGE) {
-			throw new KakaoFeignException(ExceptionInformation.KAKAO_PAGE_NUMBER_EXCEPTION);
-		}
 	}
 }
--- src/test/java/ddd/caffeine/ratrip/service/RecommendPlaceValidatorTest.java
+++ src/test/java/ddd/caffeine/ratrip/service/RecommendPlaceValidatorTest.java
@@ -6,38 +6,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 
 import ddd.caffeine.ratrip.common.exception.domain.KakaoFeignException;
-import ddd.caffeine.ratrip.module.recommend.service.KakaoFeignClient;
-import ddd.caffeine.ratrip.module.recommend.service.RecommendPlaceService;
+import ddd.caffeine.ratrip.module.recommend.service.RecommendPlaceValidator;
 
 /**
  * RecommendPlaceService 의 단위테스트 구현
  */
-class RecommendPlaceServiceTest {
+class RecommendPlaceValidatorTest {
 
-	RecommendPlaceService recommendPlaceService;
-
-	@Mock
-	KakaoFeignClient kakaoFeignClient;
+	RecommendPlaceValidator recommendPlaceValidator;
 
 	@BeforeEach
 	void init() {
-		recommendPlaceService = new RecommendPlaceService(kakaoFeignClient);
+		recommendPlaceValidator = new RecommendPlaceValidator();
 	}
 
 	@ParameterizedTest
 	@DisplayName("페이징 사이즈 유효성 검사 테스트")
 	@ValueSource(ints = {-1, 0, 46, 1000000})
 	void validatePageSizeTest(int pageSize) {
-		//given
-		String region = "서울 이태원";
-		String keyword = "볼거리";
-
 		//then
 		assertThatThrownBy(() ->
-			recommendPlaceService.recommendPlaces(region, keyword, pageSize))
+			recommendPlaceValidator.validatePageSize(pageSize))
 			.isInstanceOf(KakaoFeignException.class)
 			.hasMessage("Page 는 1 이상 45 이하 여야 합니다.");
 	}

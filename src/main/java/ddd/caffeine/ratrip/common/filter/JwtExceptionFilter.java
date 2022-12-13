e362f16f50812139e8d7e4c8bd2a69e0b7bd4f9a
jihwan <sgh002400@naver.com>
2022-12-14 00:30:31 +0900

    feat(JwtExceptionFilter): filter에서 발생하는 예외처리

--- src/main/java/ddd/caffeine/ratrip/common/filter/JwtExceptionFilter.java
+++ src/main/java/ddd/caffeine/ratrip/common/filter/JwtExceptionFilter.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ddd.caffeine.ratrip.common.exception.CommonException;
+import ddd.caffeine.ratrip.common.exception.ExceptionResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +22,26 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
 		try {
 			filterChain.doFilter(request, response);
 		} catch (CommonException e) {
-			
+			respondException(response, e);
 		}
+	}
+
+	private void respondException(HttpServletResponse response, CommonException e) throws IOException {
+		setResponseHeader(response);
+		writeResponse(response, e);
+	}
+
+	private void setResponseHeader(HttpServletResponse response) {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+	}
+
+	private void writeResponse(HttpServletResponse response, CommonException e) throws IOException {
+		ExceptionResponse exceptionResponse = ExceptionResponse.of(e.getHttpStatus(), e.getErrorCode(), e.getMessage());
+		response.getWriter().write(toJson(exceptionResponse));
+	}
+
+	private String toJson(ExceptionResponse exceptionResponse) throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(exceptionResponse);
 	}
 }

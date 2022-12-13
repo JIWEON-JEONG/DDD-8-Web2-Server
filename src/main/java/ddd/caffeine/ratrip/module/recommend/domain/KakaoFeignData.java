a2ff88cfe63a6862f41a8dab9a5186908fb89ea0
JIWEON-JEONG <dlektl6@naver.com>
2022-12-13 23:35:00 +0900

    refactor : camelCase 적용

--- src/main/java/ddd/caffeine/ratrip/module/recommend/domain/KakaoFeignData.java
+++ src/main/java/ddd/caffeine/ratrip/module/recommend/domain/KakaoFeignData.java
@@ -1,17 +1,21 @@
 package ddd.caffeine.ratrip.module.recommend.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoFeignData {
-	private String category_group_name;
-	private String category_name;
+	private String categoryGroupName;
+	private String categoryName;
 	private String phone;
-	private String place_name;
-	private String place_url;
-	private String road_address_name;
+	private String placeName;
+	private String placeUrl;
+	private String roadAddressName;
 	private String x;
 	private String y;
 }
--- src/main/java/ddd/caffeine/ratrip/module/recommend/domain/KakaoFeignMetaData.java
+++ src/main/java/ddd/caffeine/ratrip/module/recommend/domain/KakaoFeignMetaData.java
@@ -1,20 +1,25 @@
 package ddd.caffeine.ratrip.module.recommend.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoFeignMetaData {
-	private boolean is_end;
-	private int pageable_count;
-	private SameName same_name;
-	private int total_count;
+	private boolean isEnd;
+	private int pageableCount;
+	private SameName sameName;
+	private int totalCount;
 
 	@NoArgsConstructor
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	class SameName {
 		private String keyword;
 		private String[] region;
-		private String selected_region;
+		private String selectedRegion;
 	}
 }

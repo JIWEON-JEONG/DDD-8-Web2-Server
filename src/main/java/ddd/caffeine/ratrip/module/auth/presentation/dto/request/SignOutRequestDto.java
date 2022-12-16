c8b5cac3418853e3f409273dbd1dc4b3f919d6c3
jihwan <sgh002400@naver.com>
2022-12-16 16:20:52 +0900

    feat(RequestDtos): RequestDto들에 Swagger에 보여줄 설명 추가

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignOutRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignOutRequestDto.java
@@ -1,15 +1,18 @@
 package ddd.caffeine.ratrip.module.auth.presentation.dto.request;
 
 import ddd.caffeine.ratrip.module.auth.application.dto.SignOutDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignOutRequestDto {
+	@Schema(description = "엑세스 토큰", example = "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjoiMTFlZDdjMmUtZGZlZi1hZWFkLTkxM2ItZWJkOWE1N2YwMjE3IiwiZXhwIjoxNjcxMDc5NTAwfQ.pEDYlZZwAykx1wpNNRK8scnaL1SGRTTlJZ4EKC--ja5ZBg8Vz4LiyFEvSy79W1-k9PJuRKY-VCnrG8KzO4IRrQ")
 	@NotBlank(message = "Access Token must not be blank")
 	private String accessToken;
 
+	@Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NzE2ODI1MDB9.7mT7V4dmH7YDYJiTipppSRV5w8x18fEZD4mWXakKpxiRgiXfzBK-94658XRq8DJ51dr9tuzV5WScMVoXxs_GlQ")
 	@NotBlank(message = "Token must not be blank")
 	private String refreshToken;
 
--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignUpRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignUpRequestDto.java
@@ -2,15 +2,18 @@ package ddd.caffeine.ratrip.module.auth.presentation.dto.request;
 
 import ddd.caffeine.ratrip.module.auth.application.dto.SignUpDto;
 import ddd.caffeine.ratrip.module.user.domain.UserSocialType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class SignUpRequestDto {
+	@Schema(description = "카카오 토큰", example = "k5AmWU6rE9E6FRu92OP40K_MWkrk8TQJu7xaV8VLCj1zTgAAAYUT_N4T")
 	@NotBlank(message = "Token must not be blank")
 	private String token;
 
+	@Schema(description = "소셜 타입", example = "KAKAO", allowableValues = {"KAKAO", "APPLE"})
 	@NotNull(message = "Social type must not be null")
 	private UserSocialType socialType;
 

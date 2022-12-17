6ca80f5d6a30c02b009e84637cb5dafd64667dcb
jihwan <sgh002400@naver.com>
2022-12-17 23:38:11 +0900

    refactor(SignOutRequestDto): 생성자 수정 및 Getter 추가

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignOutRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignOutRequestDto.java
@@ -3,9 +3,11 @@ package ddd.caffeine.ratrip.module.auth.presentation.dto.request;
 import ddd.caffeine.ratrip.module.auth.application.dto.SignOutDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Getter
 public class SignOutRequestDto {
 	@Schema(description = "엑세스 토큰", example = "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjoiMTFlZDdjMmUtZGZlZi1hZWFkLTkxM2ItZWJkOWE1N2YwMjE3IiwiZXhwIjoxNjcxMDc5NTAwfQ.pEDYlZZwAykx1wpNNRK8scnaL1SGRTTlJZ4EKC--ja5ZBg8Vz4LiyFEvSy79W1-k9PJuRKY-VCnrG8KzO4IRrQ")
 	@NotBlank(message = "Access Token must not be blank")

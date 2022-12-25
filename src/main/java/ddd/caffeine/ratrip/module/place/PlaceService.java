92c1b9b1f0a59cae7567b4add656370ccff34e01
JIWEON-JEONG <dlektl6@naver.com>
2022-12-25 16:11:07 +0900

    docs : 예외 상황 정리 - 추후 더 추가 예정

--- src/main/java/ddd/caffeine/ratrip/module/place/PlaceService.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/PlaceService.java
@@ -6,6 +6,11 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * 1. 장소를 검색 하여, 있다면 데이터 내려주고, 없다면 api 호출.
+ *
+ * 예외
+ * - 장소가 업데이트 되었을 경우. (이사 갔거나 뿌셨거나)
+ * - 장소가 업데이트 되었는데 사진이 없을경우 -> 프론트에 기본적으로 사진 못찾음 이미지 띄워주게 해야 할 듯.
+ *
  */
 @Service
 @RequiredArgsConstructor

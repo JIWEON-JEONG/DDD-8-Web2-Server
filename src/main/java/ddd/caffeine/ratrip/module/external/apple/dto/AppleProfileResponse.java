22dacb5759da1464d1e6386593ff00d394a18452
jihwan <sgh002400@naver.com>
2022-12-27 19:52:36 +0900

    refactor(AppleProfileResponse): 유저 필드 수정

--- src/main/java/ddd/caffeine/ratrip/module/external/apple/dto/AppleProfileResponse.java
+++ src/main/java/ddd/caffeine/ratrip/module/external/apple/dto/AppleProfileResponse.java
@@ -1,12 +1,12 @@
 package ddd.caffeine.ratrip.module.external.apple.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Builder
 public class AppleProfileResponse {
 	private String id;
+	private String name;
+	private String email;
 }

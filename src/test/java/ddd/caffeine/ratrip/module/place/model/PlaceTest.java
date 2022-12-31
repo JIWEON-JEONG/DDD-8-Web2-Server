3afa8c05347918a08878b6558545e215643d0327
JIWEON-JEONG <dlektl6@naver.com>
2022-12-31 22:10:44 +0900

    refactor : 테스트코드 리팩토링

--- src/test/java/ddd/caffeine/ratrip/module/place/model/PlaceTest.java
+++ src/test/java/ddd/caffeine/ratrip/module/place/model/PlaceTest.java
@@ -13,40 +13,61 @@ class PlaceTest {
 
 	@Test
 	@DisplayName("setPlaceCategory 메서드 정상 동작 테스트")
-	void setPlaceCategoryTest() {
+	void NormalSetPlaceCategoryTest() {
 		//given
 		String cafeCategoryCode = "CE7";
-		String 기타CategoryCode = "관리 하지 않는 카테고리 코드";
 		Place placeCafe = new Place();
-		Place placeNoCategory = new Place();
 
 		//when
 		placeCafe.setPlaceCategory(cafeCategoryCode);
-		placeNoCategory.setPlaceCategory(기타CategoryCode);
 
 		//then
 		Assertions.assertThat(placeCafe.getCategory()).isEqualTo(Category.CAFE);
+	}
+
+	@Test
+	@DisplayName("setPlaceCategory 관리하지 않는 카테고리일 경우 동작 테스트")
+	void 기타SetPlaceCategoryTest() {
+		//given
+		String 기타CategoryCode = "관리 하지 않는 카테고리 코드";
+		Place placeNoCategory = new Place();
+
+		//when
+		placeNoCategory.setPlaceCategory(기타CategoryCode);
+
+		//then
 		Assertions.assertThat(placeNoCategory.getCategory()).isEqualTo(Category.기타);
 	}
 
 	@Test
 	@DisplayName("setAddress 메서드 정상 동작 테스트")
-	void setAddressTest() {
+	void normalSetAddressTest() {
 		//given
 		String 제주Address = "제주특별자치도 제주시 외도일동 640-2";
-		String 기타Address = "XX도 제주시 외도일동 640-2";
 		Place 제주Place = new Place();
-		Place 기타Place = new Place();
 
 		//when
 		제주Place.setAddress(제주Address);
-		기타Place.setAddress(기타Address);
 
 		//then
 		Address 제주 = 제주Place.getAddress();
-		Address 기타 = 기타Place.getAddress();
 		Assertions.assertThat(제주.getRegion()).isEqualTo(Region.제주특별자치도);
 		Assertions.assertThat(제주.getDetailed()).isEqualTo("제주시 외도일동 640-2");
+	}
+
+	@Test
+	@DisplayName("setAddress 관리하지 않는 Region 일 경우 동작 테스트")
+	void 기타SetAddressTest() {
+		//given
+		String 기타Address = "XX도 제주시 외도일동 640-2";
+		Place 기타Place = new Place();
+
+		//when
+		기타Place.setAddress(기타Address);
+
+		//then
+		Address 기타 = 기타Place.getAddress();
 		Assertions.assertThat(기타.getRegion()).isEqualTo(Region.기타);
+		Assertions.assertThat(기타.getDetailed()).isEqualTo("제주시 외도일동 640-2");
 	}
 }

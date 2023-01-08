ef5c047175ba7fe671491f88cfe7ba6c4b8e901c
JIWEON-JEONG <dlektl6@naver.com>
2023-01-08 19:10:26 +0900

    remove : transient field

--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/TravelPlan.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/TravelPlan.java
@@ -1,6 +1,5 @@
 package ddd.caffeine.ratrip.module.travel_plan;
 
-import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -8,10 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 import ddd.caffeine.ratrip.module.Region;
-import ddd.caffeine.ratrip.module.user.domain.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -30,12 +27,4 @@ public class TravelPlan {
 
 	@Column
 	private int travelDays;
-
-	/**
-	 * getter 같은 메서드 사용시 쿼리 나가지 않는지 확인.
-	 *
-	 * Todo: 최대 몇명까지 초대 할 수 있는지 정하는게 좋을거같다. 악용방지.
-	 */
-	@Transient
-	private List<User> users;
 }

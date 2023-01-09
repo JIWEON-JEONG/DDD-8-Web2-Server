978edb862b96618acd0f96923cb45e147aa9fbf2
jihwan <sgh002400@naver.com>
2023-01-09 12:13:55 +0900

    refactor(KakaoBearerTokenRequest): 생성자 추가 및 스네이크 케이스로 변경

--- src/main/java/ddd/caffeine/ratrip/module/auth/external/dto/request/KakaoBearerTokenRequest.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/external/dto/request/KakaoBearerTokenRequest.java
@@ -4,17 +4,20 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@AllArgsConstructor
+@Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@AllArgsConstructor
 public class KakaoBearerTokenRequest {
 	private static final String GRANT_TYPE = "authorization_code";
-	private final String grantType;
-	private final String clientId;
-	private final String redirectUri;
-	private final String code;
+	private String grant_type;
+	private String client_id;
+	private String redirect_uri;
+	private String code;
+	private String client_secret;
 
 	public static KakaoBearerTokenRequest of(String clientId, String redirectUri, String code) {
-		return new KakaoBearerTokenRequest(GRANT_TYPE, clientId, redirectUri, code);
+		return new KakaoBearerTokenRequest(GRANT_TYPE, clientId, redirectUri, code, null);
 	}
 }

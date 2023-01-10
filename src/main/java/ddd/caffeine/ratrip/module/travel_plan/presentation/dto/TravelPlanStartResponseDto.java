e03c29c2214212461e0cf88885f7b81ee3ab05bd
JIWEON-JEONG <dlektl6@naver.com>
2023-01-10 20:26:19 +0900

    refactor : test yml 설정

--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/TravelPlanStartResponseDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/TravelPlanStartResponseDto.java
@@ -1,10 +1,12 @@
 package ddd.caffeine.ratrip.module.travel_plan.presentation.dto;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class TravelPlanStartResponseDto {
-	String message = "success";
+	private UUID planUUID;
 }

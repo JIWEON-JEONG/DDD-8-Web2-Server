911d2d176ae88c829dda693ccd19ce6fe758cd62
JIWEON-JEONG <dlektl6@naver.com>
2023-01-11 17:44:29 +0900

    pull upstream main

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,12 @@
+insert into place values (11ed918a-19bb-b680-b4ea-8962b55ef5c2,
+                          "test", "name", "category", "region", "detailed",
+                          11.11, 11.11, false, false, "testLInk", "1", )
+-- insert into place values (11ed918a-19be-ead1-b4ea-e77e3e7c3afe,)
+-- insert into place values (11ed918a-19be-ead2-b4ea-cd00d9d3cbe7,)
+-- insert into place values (11ed918a-19be-ead3-b4ea-81ce536b4207,)
+-- insert into place values (11ed918a-19bf-11e4-b4ea-8f0c7fcaa607,)
+-- insert into place values (11ed918a-19bf-11e5-b4ea-53d45d6399d3,)
+-- insert into place values (11ed918a-19bf-11e6-b4ea-e743c0a2b792,)
+-- insert into place values (11ed918a-19bf-38f7-b4ea-2d8b1db3ed69,)
+-- insert into place values (11ed918a-19bf-38f8-b4ea-1b5ae812bc11,)
+-- insert into place values (11ed918a-19bf-38f9-b4ea-af8e36226d97,)

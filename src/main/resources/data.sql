debda0b39bcba2fcb7c362142274a6f9512ad6e9
JIWEON-JEONG <dlektl6@naver.com>
2023-01-12 03:05:53 +0900

    feat : insert 더미 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,50 +1,46 @@
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bb-b680-b4ea-8962b55ef5c2", "-", "")),
         "충남 천안시 서북구 쌍용동 541-4", "충청남도", "RESTAURANT", "", false, false,
-        "1287983816", 36.7977617724395, 127.132441603269, "맛집연구소한식&카페", 1, "041-573-2025");
+        "1287983816", 127.132441603269, 36.7977617724395, "맛집연구소한식&카페", 1, "041-573-2025");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f8-b4ea-1b5ae812bc11", "-", "")),
         "서울 구로구 신도림동 432-30", "서울특별시", "ACCOMMODATION", "", false, false,
-        "1212235250", 126.88540809078883, 37.506232324972515, "라마다서울신도림호텔", 1, "02-2162-2000");
-
+        "1212235250", 37.506232324972515, 126.88540809078883, "라마다서울신도림호텔", 1, "02-2162-2000");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f9-b4ea-af8e36226d97", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 강서구 마곡동 727-256", "서울특별시", "TOURIST_ATTRACTION", "", false, false,
+        "1164966384", 37.5693958477101, 126.835037244018, "서울식물원", 1, "02-2104-9716");
+
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f7-b4ea-2d8b1db3ed69", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 강남구 논현동 254-23", "서울특별시", "RESTAURANT", "", false, false,
+        "11594198", 37.5137410270759, 127.041235508952, "남도음식토말 본점", 1, "02-518-0292");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e6-b4ea-e743c0a2b792", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 용산구 이태원동 108-3", "서울특별시", "TOURIST_ATTRACTION", "", false, false,
+        "27302721", 37.5350045487442, 126.994573687665, "세계음식특화거리", 1, "");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e5-b4ea-53d45d6399d3", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 서초구 양재동 263-15", "서울특별시", "RESTAURANT", "", false, false,
+        "153076406", 37.4763054694377, 127.042716287433, "사찰음식도반", 1, "02-1644-1803");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e4-b4ea-8f0c7fcaa607", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 관악구 봉천동 36-20", "서울특별시", "RESTAURANT", "", false, false,
+        "16711942", 37.485419798945, 126.956523232034, "남도음식전문점", 1, "02-884-5458");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19be-ead3-b4ea-81ce536b4207", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 마포구 용강동 45-15", "서울특별시", "TOURIST_ATTRACTION", "", false, false,
+        "12576887", 37.5415853568029, 126.941201488128, "마포음식문화거리", 1, "");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19be-ead2-b4ea-cd00d9d3cbe7", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");
+        "서울 서초구 양재동 14-4", "서울특별시", "CAFE", "", false, false,
+        "25985341", 37.4822865420713, 127.036849465801, "스타벅스 양재역점", 1, "1522-3232");
 
-insert into place
-values (UNHEX(REPLACE("11ed918a-19be-ead1-b4ea-e77e3e7c3afe", "-", "")),
-        "detailed_address", "서울특별시", "CAFE", "imageLink", false, false,
-        "kakaoId", 11.11, 11.11, "name", 10, "testPhone");

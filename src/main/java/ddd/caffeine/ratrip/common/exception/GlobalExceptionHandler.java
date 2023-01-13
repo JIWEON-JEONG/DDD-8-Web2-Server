c9d82ed2f01aaf58a3d37c57a6948132a13c70d1
jihwan <sgh002400@naver.com>
2023-01-13 13:02:50 +0900

    refactor(GlobalExceptionHandler): ResponseEntityExceptionHandler 사용을 위한 커스텀 예외 핸들러 삭제

--- src/main/java/ddd/caffeine/ratrip/common/exception/GlobalExceptionHandler.java
+++ src/main/java/ddd/caffeine/ratrip/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,7 @@
 package ddd.caffeine.ratrip.common.exception;
 
-import java.net.BindException;
-
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MissingRequestValueException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -53,82 +48,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		ExceptionResponse response = ExceptionResponse.builder()
 			.httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 			.errorCode("UNEXPECTED_EXCEPTION")
-			.message(e.getMessage())
-			.build();
-
-		return new ResponseEntity<>(response, response.getHttpStatus());
-	}
-
-	/**
-	 * 400 Bad Request
-	 * Spring Validation 에서 발생하는 Exception.
-	 *
-	 * @param e BindException
-	 * @return ErrorResponse
-	 */
-	@ExceptionHandler(BindException.class)
-	public ResponseEntity<ExceptionResponse> handleBindException(BindException e) {
-		log.error("cause : {}, message : {}", e.getCause(), e.getMessage());
-		ExceptionResponse response = ExceptionResponse.builder()
-			.httpStatus(HttpStatus.BAD_REQUEST)
-			.errorCode("BIND_EXCEPTION")
-			.message(e.getMessage())
-			.build();
-
-		return new ResponseEntity<>(response, response.getHttpStatus());
-	}
-
-	/**
-	 * 400 Bad Request
-	 * JSON 형식이 잘못되었을 때 발생하는 Exception. (Postman에서 테스트할 때 Json이 아닌 Text로 보내면 발생)
-	 *
-	 * @param e HttpMessageNotReadableException
-	 * @return ErrorResponse
-	 */
-	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-		log.error("cause : {}, message : {}", e.getCause(), e.getMessage());
-		ExceptionResponse response = ExceptionResponse.builder()
-			.httpStatus(HttpStatus.BAD_REQUEST)
-			.errorCode("HTTP_MESSAGE_NOT_READABLE_EXCEPTION")
-			.message(e.getMessage())
-			.build();
-
-		return new ResponseEntity<>(response, response.getHttpStatus());
-	}
-
-	/**
-	 * 400 BadRequest
-	 * RequestParam, RequestPath, RequestPart 등의 필드가 입력되지 않은 경우 발생하는 Exception
-	 *
-	 * @param e MissingRequestValueException
-	 * @return ErrorResponse
-	 */
-	@ExceptionHandler(MissingRequestValueException.class)
-	public ResponseEntity<ExceptionResponse> handleMissingRequestValueException(MissingRequestValueException e) {
-		log.error("cause : {}, message : {}", e.getCause(), e.getMessage());
-		ExceptionResponse response = ExceptionResponse.builder()
-			.httpStatus(HttpStatus.BAD_REQUEST)
-			.errorCode("MISSING_REQUEST_VALUE_EXCEPTION")
-			.message(e.getMessage())
-			.build();
-
-		return new ResponseEntity<>(response, response.getHttpStatus());
-	}
-
-	/**
-	 * 400 BadRequest
-	 * 파라미터 바인딩 시점에 타입이 맞지 않으면 내부적으로 발생하는 Exception
-	 *
-	 * @param e TypeMismatchException
-	 * @return ErrorResponse
-	 */
-	@ExceptionHandler(TypeMismatchException.class)
-	public ResponseEntity<ExceptionResponse> handleTypeMismatchException(TypeMismatchException e) {
-		log.error("cause : {}, message : {}", e.getCause(), e.getMessage());
-		ExceptionResponse response = ExceptionResponse.builder()
-			.httpStatus(HttpStatus.BAD_REQUEST)
-			.errorCode("TYPE_MISMATCH_EXCEPTION")
 			.message(e.getMessage())
 			.build();
 

d1b2901af7c79c3e61bad4b335cd0695786dc307
JIWEON-JEONG <dlektl6@naver.com>
2023-01-15 03:05:31 +0900

    fix : data.sql

--- src/main/resources/data-develop.sql
+++ src/main/resources/data-develop.sql
@@ -1,11 +1,13 @@
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f8-b4ea-1b5ae812bc11", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 구로구 신도림동 432-30", "서울특별시", "ACCOMMODATION",
         "https://search.pstatic.net/common/?src=http://imgnews.naver.net/image/5629/2019/09/19/0000174113_001_20190919170750460.png&type=b150",
         false, false, "1212235250", 37.506232324972515, 126.88540809078883, "라마다서울신도림호텔", 1, "02-2162-2000");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f9-b4ea-af8e36226d97", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 강서구 마곡동 727-256", "서울특별시", "TOURIST_ATTRACTION",
         "http://imgnews.naver.net/image/088/2018/10/11/0000556942_005_20181011104557550.jpg", false, false,
         "1164966384", 37.5693958477101, 126.835037244018, "서울식물원", 1, "02-2104-9716");
@@ -13,12 +15,14 @@ values (UNHEX(REPLACE("11ed918a-19bf-38f9-b4ea-af8e36226d97", "-", "")),
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-38f7-b4ea-2d8b1db3ed69", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 강남구 논현동 254-23", "서울특별시", "RESTAURANT",
         "http://tong.visitkorea.or.kr/cms/resource/21/2870721_image2_1.jpg",
         false, false, "11594198", 37.5137410270759, 127.041235508952, "남도음식토말 본점", 1, "02-518-0292");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e6-b4ea-e743c0a2b792", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 용산구 이태원동 108-3", "서울특별시", "TOURIST_ATTRACTION",
         "https://search.pstatic.net/sunny/?src=https://i.pinimg.com/736x/79/2d/4c/792d4cd39dd7fdf5afb4c57161d2274b.jpg&type=b150",
         false, false,
@@ -26,6 +30,7 @@ values (UNHEX(REPLACE("11ed918a-19bf-11e6-b4ea-e743c0a2b792", "-", "")),
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e5-b4ea-53d45d6399d3", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 서초구 양재동 263-15", "서울특별시", "RESTAURANT",
         "http://post.phinf.naver.net/MjAyMTA1MTdfMjgx/MDAxNjIxMjM5NDI2Mjk1.ZbaLv4tY5C_ixuchpWuhAxstOeVL3-2qhjfKQYpprk4g.7s9PSZVUuW0VARtM0cmtUOpU1h67TOQ5MsSYr_6N-mYg.JPEG/I6dMO2SBfa8s7LStaph6w8woi4RQ.jpg",
         false, false,
@@ -33,6 +38,7 @@ values (UNHEX(REPLACE("11ed918a-19bf-11e5-b4ea-53d45d6399d3", "-", "")),
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19bf-11e4-b4ea-8f0c7fcaa607", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 관악구 봉천동 36-20", "서울특별시", "RESTAURANT",
         "http://post.phinf.naver.net/MjAxODAyMjdfMjgg/MDAxNTE5NzE5ODUzNjEz.6PMlGLC5P9IVhOpCtnlIpH-zqGvlCNqSPZa627QX2_kg.ZCD-SEjHuV3gH-PKwQhnJkAyXOeAlY0L5pWM7VFFZAAg.PNG/IvPxspXhdCZZVrRZ1-U5XIDOmWV0.jpg",
         false, false, "16711942", 37.485419798945, 126.956523232034, "남도음식전문점", 1, "02-884-5458");
@@ -40,12 +46,14 @@ values (UNHEX(REPLACE("11ed918a-19bf-11e4-b4ea-8f0c7fcaa607", "-", "")),
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19be-ead3-b4ea-81ce536b4207", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 마포구 용강동 45-15", "서울특별시", "TOURIST_ATTRACTION",
         "https://mp-seoul-image-production-s3.mangoplate.com/47290/bm8jg27x8j6ukd.jpg?fit=around%7C512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
         false, false, "12576887", 37.5415853568029, 126.941201488128, "마포음식문화거리", 1, "");
 
 insert into place
 values (UNHEX(REPLACE("11ed918a-19be-ead2-b4ea-cd00d9d3cbe7", "-", "")),
+        "1000-01-01 00:00:00", " 1000-01-01 00:00:00",
         "서울 서초구 양재동 14-4", "서울특별시", "CAFE",
         "http://image-logo.alba.kr/%2Fdata_image2%2Fcomlogo%2F202110%2F2021101910473675292_0_m.jpg",
         false, false, "25985341", 37.4822865420713, 127.036849465801, "스타벅스 양재역점", 1, "1522-3232");

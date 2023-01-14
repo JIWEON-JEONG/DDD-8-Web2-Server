31d3911dc1ec202ce5bc9ed89c0998159f4b2836
jihwan <sgh002400@naver.com>
2023-01-14 23:00:57 +0900

    refactor(UserRepository): JpaRepository에 PK 타입으로 변경

--- src/main/java/ddd/caffeine/ratrip/module/user/domain/repository/UserRepository.java
+++ src/main/java/ddd/caffeine/ratrip/module/user/domain/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ddd.caffeine.ratrip.module.user.domain.SocialInfo;
 import ddd.caffeine.ratrip.module.user.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 	User findUserBySocialInfo(SocialInfo socialInfo);
 
 	User findUserById(UUID id); //TEST METHOD

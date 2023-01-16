0d43e6b937bfd9062d31fcd3f4f17d083127b309
JIWEON-JEONG <dlektl6@naver.com>
2023-01-17 02:48:28 +0900

    feat : Custom 검증 애너테이션 구현

---
+++ src/main/java/ddd/caffeine/ratrip/common/validator/annotation/UUIDValidator.java
@@ -0,0 +1,18 @@
+package ddd.caffeine.ratrip.common.validator.annotation;
+
+import java.util.regex.Pattern;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UUIDValidator implements ConstraintValidator<Number, String> {
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		final Pattern UUID_PATTERN = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
+
+		if (!(UUID_PATTERN.matcher(value).matches())) {
+			return Boolean.FALSE;
+		}
+		return Boolean.TRUE;
+	}
+}

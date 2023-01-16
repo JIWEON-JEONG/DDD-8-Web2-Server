9a07a01fef01c7e50a4e7a719e7e5a3a96263182
jihwan <sgh002400@naver.com>
2023-01-16 12:35:10 +0900

    feat(UserException): UserException 생성

---
+++ src/main/java/ddd/caffeine/ratrip/common/exception/domain/UserException.java
@@ -0,0 +1,15 @@
+package ddd.caffeine.ratrip.common.exception.domain;
+
+import ddd.caffeine.ratrip.common.exception.BaseException;
+import ddd.caffeine.ratrip.common.exception.ExceptionInformation;
+
+public class UserException extends BaseException {
+
+	public UserException(int status, String errorCode, String message) {
+		super(status, errorCode, message);
+	}
+
+	public UserException(ExceptionInformation information) {
+		super(information);
+	}
+}

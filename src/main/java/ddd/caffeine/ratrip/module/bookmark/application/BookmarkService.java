9efe5115085d3171852aa1b605bc3ec818988d6b
jihwan <sgh002400@naver.com>
2023-01-16 12:38:26 +0900

    feat(BookmarkService): 북마크 생성 로직 추가

--- src/main/java/ddd/caffeine/ratrip/module/bookmark/application/BookmarkService.java
+++ src/main/java/ddd/caffeine/ratrip/module/bookmark/application/BookmarkService.java
@@ -5,18 +5,26 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ddd.caffeine.ratrip.module.bookmark.domain.Bookmark;
 import ddd.caffeine.ratrip.module.bookmark.domain.repository.BookmarkRepository;
+import ddd.caffeine.ratrip.module.place.model.Place;
+import ddd.caffeine.ratrip.module.place.service.PlaceService;
+import ddd.caffeine.ratrip.module.user.application.UserService;
+import ddd.caffeine.ratrip.module.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookmarkService {
+	private final UserService userService;
+	private final PlaceService placeService;
 	private final BookmarkRepository bookmarkRepository;
-	private final BookmarkValidator bookmarkValidator;
 
 	public UUID addBookmark(final UUID placeId) {
-
-		return UUID.randomUUID();
+		Place place = placeService.findPlaceById(placeId);
+		User user = userService.findUserById(UUID.randomUUID()); //TODO - 리팩토링 필요
+		Bookmark bookmark = bookmarkRepository.save(Bookmark.of(user, place));
+		return bookmark.getId(); //TODO - 북마크 ID 리턴하는거 주안님이랑 상의
 	}
 }

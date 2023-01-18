e66d53260ab223b390890ef6ca561e6c7c9abbca
jihwan <sgh002400@naver.com>
2023-01-19 03:16:25 +0900

    fix(NotificationRepository): PK 타입 변경

--- src/main/java/ddd/caffeine/ratrip/module/notification/domain/respository/NotificationRepository.java
+++ src/main/java/ddd/caffeine/ratrip/module/notification/domain/respository/NotificationRepository.java
@@ -1,10 +1,8 @@
 package ddd.caffeine.ratrip.module.notification.domain.respository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ddd.caffeine.ratrip.module.notification.domain.Notification;
 
-public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }

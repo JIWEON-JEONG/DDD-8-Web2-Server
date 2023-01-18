bb70681d3dcd2b4e3fc348fc417adaf007274262
jihwan <sgh002400@naver.com>
2023-01-18 23:54:37 +0900

    feat(Notification): 공지사항 엔티티 생성

---
+++ src/main/java/ddd/caffeine/ratrip/module/notification/Notification.java
@@ -0,0 +1,26 @@
+package ddd.caffeine.ratrip.module.notification;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import ddd.caffeine.ratrip.common.jpa.AuditingTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends AuditingTimeEntity {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(columnDefinition = "VARCHAR(100)")
+	private String title;
+
+	@Column(columnDefinition = "VARCHAR(5000)")
+	private String content;
+}

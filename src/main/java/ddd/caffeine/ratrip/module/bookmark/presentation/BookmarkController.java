e4b1a97b7574ee5b55ab7567f7d3dda0fa09ab58
jihwan <sgh002400@naver.com>
2023-01-18 15:37:52 +0900

    feat(BookmarkController): 북마크 리스트 조회 기본값 설정

--- src/main/java/ddd/caffeine/ratrip/module/bookmark/presentation/BookmarkController.java
+++ src/main/java/ddd/caffeine/ratrip/module/bookmark/presentation/BookmarkController.java
@@ -20,6 +20,7 @@ import ddd.caffeine.ratrip.module.bookmark.application.BookmarkService;
 import ddd.caffeine.ratrip.module.bookmark.presentation.dto.response.BookmarksResponseDto;
 import ddd.caffeine.ratrip.module.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,22 +31,25 @@ public class BookmarkController {
 
 	@Operation(summary = "[인증] 북마크 리스트 조회")
 	@GetMapping("")
-	public ResponseEntity<BookmarksResponseDto> getBookmarks(@AuthenticationPrincipal User user,
+	public ResponseEntity<BookmarksResponseDto> getBookmarks(
+		@Parameter(hidden = true) @AuthenticationPrincipal User user,
 		@RequestParam(name = "category", required = false) List<String> categories,
 		//TODO - 인자를 Enum 타입으로 받는 법 알아보기
-		@PageableDefault(size = 3, sort = "name", direction = Sort.Direction.DESC) Pageable pageable) { //TODO - direction = Sort.Direction.DESC이 뭐지
+		@PageableDefault(size = 7, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 		return ResponseEntity.ok(bookmarkService.getBookmarks(user, categories, pageable));
 	}
 
 	@Operation(summary = "[인증] 북마크 추가")
 	@PostMapping("/{placeId}")
-	public ResponseEntity<UUID> addBookmark(@PathVariable UUID placeId, @AuthenticationPrincipal User user) {
+	public ResponseEntity<UUID> addBookmark(@PathVariable UUID placeId,
+		@Parameter(hidden = true) @AuthenticationPrincipal User user) {
 		return ResponseEntity.ok(bookmarkService.addBookmark(placeId, user));
 	}
 
 	@Operation(summary = "[인증] 북마크 삭제")
 	@DeleteMapping("/{placeId}")
-	public ResponseEntity<Void> deleteBookmark(@PathVariable UUID placeId, @AuthenticationPrincipal User user) {
+	public ResponseEntity<Void> deleteBookmark(@PathVariable UUID placeId,
+		@Parameter(hidden = true) @AuthenticationPrincipal User user) {
 		bookmarkService.deleteBookmark(placeId, user);
 		return ResponseEntity.ok().build();
 	}

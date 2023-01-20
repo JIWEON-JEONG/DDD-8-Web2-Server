d8c26d901173ab5230c906a1acfa4ef4a4c04063
JIWEON-JEONG <dlektl6@naver.com>
2023-01-20 12:36:35 +0900

    test : 장소 순서 편집 테스트

--- src/test/java/ddd/caffeine/ratrip/module/travel_plan/domain/DaySchedulePlaceTest.java
+++ src/test/java/ddd/caffeine/ratrip/module/travel_plan/domain/DaySchedulePlaceTest.java
@@ -1,5 +1,7 @@
 package ddd.caffeine.ratrip.module.travel_plan.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,18 @@ class DaySchedulePlaceTest {
 	@DisplayName("exchangeOrder 메서드 정상 동작 테스트")
 	void exchangeOrderTest() {
 		//given
-		DaySchedulePlace basicDaySchedulePlace = new DaySchedulePlace();
+		DaySchedulePlace baseDaySchedulePlace = DaySchedulePlace.builder()
+			.order(1)
+			.build();
+		DaySchedulePlace exchangeDaySchedulePlace = DaySchedulePlace.builder()
+			.order(2)
+			.build();
+
+		//when
+		baseDaySchedulePlace.exchangeOrder(exchangeDaySchedulePlace);
+
+		//then
+		assertThat(baseDaySchedulePlace.getOrder()).isEqualTo(2);
+		assertThat(exchangeDaySchedulePlace.getOrder()).isEqualTo(1);
 	}
 }

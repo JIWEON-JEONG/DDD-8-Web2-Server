98802f6161879f6cce548f8c0c60cee5f4a078c3
jihwan <sgh002400@naver.com>
2023-01-23 22:47:07 +0900

    feat(SignInWithAppleDto): properties 변경

--- src/main/java/ddd/caffeine/ratrip/module/auth/application/dto/SignInWithAppleDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/application/dto/SignInWithAppleDto.java
@@ -1,4 +1,18 @@
 package ddd.caffeine.ratrip.module.auth.application.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class SignInWithAppleDto {
+	private String idToken;
+	private String authorizationCode;
+	private String state;
+	private String user;
+
+	public static SignInWithAppleDto of(String idToken, String authorizationCode, String state, String user) {
+		return new SignInWithAppleDto(idToken, authorizationCode, state, user);
+	}
 }
--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignInWithAppleRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignInWithAppleRequestDto.java
@@ -2,6 +2,7 @@ package ddd.caffeine.ratrip.module.auth.presentation.dto.request;
 
 import javax.validation.constraints.NotBlank;
 
+import ddd.caffeine.ratrip.module.auth.application.dto.SignInWithAppleDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,21 @@ import lombok.NoArgsConstructor;
 public class SignInWithAppleRequestDto {
 	@Schema(description = "아이디 토큰", example = "k5AmWU6rE9E6FRu92OP40K_MWkrk8TQJu7xaV8VLCj1zTgAAAYUT_N4T")
 	@NotBlank(message = "Token must not be blank")
-	private String token;
+	private String idToken;
 
-	public SignInWithKakaoDto toServiceDto() {
-		return SignInWithKakaoDto.of(token);
+	@Schema(description = "인증 코드", example = "")
+	@NotBlank(message = "Authorization code must not be blank")
+	private String authorizationCode;
+
+	@Schema(description = "상태", example = "")
+	@NotBlank(message = "State must not be blank")
+	private String state;
+
+	@Schema(description = "사용자", example = "")
+	@NotBlank(message = "User must not be blank")
+	private String user;
+
+	public SignInWithAppleDto toServiceDto() {
+		return SignInWithAppleDto.of(idToken, authorizationCode, state, user);
 	}
 }

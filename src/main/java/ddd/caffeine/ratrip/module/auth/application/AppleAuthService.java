edd2570b9cb9d1dc8a201c5ace9ae877ab84af65
jihwan <sgh002400@naver.com>
2023-01-24 00:18:32 +0900

    refactor(AppleAuthService): 파라미터 네이밍 수정

--- src/main/java/ddd/caffeine/ratrip/module/auth/application/AppleAuthService.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/application/AppleAuthService.java
@@ -1,14 +1,9 @@
 package ddd.caffeine.ratrip.module.auth.application;
 
-import java.util.UUID;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ddd.caffeine.ratrip.module.auth.application.dto.SignUpWithAppleDto;
-import ddd.caffeine.ratrip.module.auth.external.apple.AppleTokenProvider;
-import ddd.caffeine.ratrip.module.auth.presentation.dto.response.SignInResponseDto;
-import ddd.caffeine.ratrip.module.auth.presentation.dto.response.TokenResponseDto;
+import ddd.caffeine.ratrip.common.util.AppleTokenProvider;
 import ddd.caffeine.ratrip.module.user.application.UserService;
 import ddd.caffeine.ratrip.module.user.domain.UserSocialType;
 import lombok.RequiredArgsConstructor;
@@ -22,23 +17,7 @@ public class AppleAuthService {
 	private final UserService userService;
 	private final TokenService tokenService;
 
-	public SignInResponseDto signUpWithApple(SignUpWithAppleDto request) {
-		String socialId = getAppleProfileResponse(request.getToken());
-		UUID userId = userService.registerUser(request.registerUserUsedByAppleAuth(socialId, socialType));
-		TokenResponseDto tokenResponseDto = tokenService.createTokenInfo(userId);
-
-		return SignInResponseDto.of(userId, tokenResponseDto);
-	}
-
-	public SignInResponseDto signInWithApple(SignInWithKakaoDto request) {
-		String socialId = getAppleProfileResponse(request.getToken());
-		UUID userId = userService.findUserBySocialIdAndSocialType(socialId, socialType);
-		TokenResponseDto tokenResponseDto = tokenService.createTokenInfo(userId);
-
-		return SignInResponseDto.of(userId, tokenResponseDto);
-	}
-
-	private String getAppleProfileResponse(String token) {
-		return appleTokenProvider.getSocialIdFromIdToken(token);
+	public String getAppleProfileResponse(String idToken) {
+		return appleTokenProvider.getSocialIdFromIdToken(idToken);
 	}
 }

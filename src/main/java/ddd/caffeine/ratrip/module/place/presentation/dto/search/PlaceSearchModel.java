5fe12fa23613fa23c23776ece42e6ceb6769e561
JIWEON-JEONG <dlektl6@naver.com>
2023-01-25 03:05:40 +0900

    fix: 필드 변경

--- src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/search/PlaceSearchModel.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/search/PlaceSearchModel.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class PlaceSearchModel {
-	private String id;
+	private String kakaoId;
 	private String name;
 	private String address;
 	private String latitude;

1355dc9b5a66a15a06a2e723dacc68426ffa3023
jihwan <sgh002400@naver.com>
2023-01-25 16:36:25 +0900

    delete(AuthController): 불필요한 메서드 및 변수 삭제

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/AuthController.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/AuthController.java
@@ -1,10 +1,7 @@
 package ddd.caffeine.ratrip.module.auth.presentation;
 
-import java.util.UUID;
-
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,31 +25,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-	/**
-	 * 아래 값들은 테스트를 위한 값으로 테스트 이후 삭제할 예정
-	 */
-	@Value("${APPLE_CLIENT_ID}")
-	private String appleClientId;
-
-	@Value("${APPLE_REDIRECT_URI}")
-	private String appleRedirectUri;
-
 	private final AuthService authService;
 
 	@Operation(summary = "카카오 로그인")
 	@GetMapping("/signin/kakao")
 	public ResponseEntity<SignInResponseDto> signInWithKakao(@RequestParam("code") String code) {
 		return ResponseEntity.ok(authService.signInWithKakao(code));
-	}
-
-	@Operation(summary = "[테스트용] 애플 로그인 API 호출 (테스트 이후 삭제 예정)")
-	@GetMapping("/call")
-	public String call() {
-		final String redirectUrl =
-			"https://appleid.apple.com/auth/authorize?" + "client_id=" + appleClientId + "&redirect_uri="
-				+ appleRedirectUri + "&response_type=code%20id_token&scope=name%20email&response_mode=form_post&nonce="
-				+ UUID.randomUUID();
-		return "redirect:" + redirectUrl;
 	}
 
 	@Operation(summary = "애플 로그인")

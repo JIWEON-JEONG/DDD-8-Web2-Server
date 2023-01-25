b2f43b091116d346688aad6e601170001d118275
JIWEON-JEONG <dlektl6@naver.com>
2023-01-25 12:34:46 +0900

    refactor : 북마크 생성 Put 메서드로 변경

--- src/main/java/ddd/caffeine/ratrip/module/place/presentation/PlaceController.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/presentation/PlaceController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -95,7 +96,7 @@ public class PlaceController {
 
 	@Operation(summary = "[인증] 북마크 추가")
 	@ApiResponse(description = "북마크 추가 성공 시, 북마크 ID 반환")
-	@PostMapping("/{id}/bookmarks")
+	@PutMapping("/{id}/bookmarks")
 	public ResponseEntity<BookmarkAddResponseDto> callAddBookmarkApi(
 		@Parameter(hidden = true) @AuthenticationPrincipal User user,
 		@PathVariable @UUIDFormat String id) {

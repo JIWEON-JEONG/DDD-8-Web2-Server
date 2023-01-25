efbf970669564113a53e9b073972f7d398310321
JIWEON-JEONG <dlektl6@naver.com>
2023-01-25 13:29:48 +0900

    feat : 이미 저장된 장소를 추가 할경우 처리

--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/application/DaySchedulePlaceService.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/application/DaySchedulePlaceService.java
@@ -1,10 +1,13 @@
 package ddd.caffeine.ratrip.module.travel_plan.application;
 
+import static ddd.caffeine.ratrip.common.exception.ExceptionInformation.*;
+
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import ddd.caffeine.ratrip.common.exception.domain.DayScheduleException;
 import ddd.caffeine.ratrip.module.place.domain.Place;
 import ddd.caffeine.ratrip.module.travel_plan.domain.DaySchedule;
 import ddd.caffeine.ratrip.module.travel_plan.domain.DaySchedulePlace;
@@ -23,6 +26,7 @@ public class DaySchedulePlaceService {
 	}
 
 	public UUID addPlace(DaySchedule daySchedule, Place place, String memo) {
+		validateAddPlace(daySchedule, place);
 		DaySchedulePlace daySchedulePlace = DaySchedulePlace.builder()
 			.daySchedule(daySchedule)
 			.place(place)
@@ -44,5 +48,13 @@ public class DaySchedulePlaceService {
 	private int readNextSequence(UUID dayScheduleUUID) {
 		int total = daySchedulePlaceRepository.countPlacesByDayScheduleUUID(dayScheduleUUID);
 		return total + 1;
+	}
+
+	private void validateAddPlace(DaySchedule daySchedule, Place place) {
+		DaySchedulePlace daySchedulePlace = daySchedulePlaceRepository.findByDayScheduleAndPlace(daySchedule,
+			place);
+		if (daySchedule != null) {
+			throw new DayScheduleException(ALREADY_EXIST_PLACE_IN_SCHEDULE_EXCEPTION);
+		}
 	}
 }

3b2bd818d8ffe6376cae98ea0ef69817961997b3
jihwan <sgh002400@naver.com>
2023-01-26 15:33:34 +0900

    refactor(AuthController): 파라미터 Dto로 받게 변경

--- src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignInWithAppleRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/auth/presentation/dto/request/SignInWithAppleRequestDto.java
@@ -1,5 +1,7 @@
 package ddd.caffeine.ratrip.module.auth.presentation.dto.request;
 
+import javax.validation.constraints.NotBlank;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import ddd.caffeine.ratrip.module.auth.application.dto.SignInWithAppleDto;
@@ -11,18 +13,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class SignInWithAppleRequestDto {
 	@Schema(description = "아이디 토큰", example = "k5AmWU6rE9E6FRu92OP40K_MWkrk8TQJu7xaV8VLCj1zTgAAAYUT_N4T")
-	//@NotBlank(message = "Token must not be blank")
+	@NotBlank(message = "Token must not be blank")
 	@JsonProperty("id_token")
 	private String idToken;
 
 	@Schema(description = "인증 코드", example = "cs27903241f4a4cf395ba97456570b048.0.rafsfr.Z6E5z7DOSgRRrZXRmdybDQ")
-	//@NotBlank(message = "Authorization code must not be blank")
+	@NotBlank(message = "Authorization code must not be blank")
 	@JsonProperty("code")
 	private String authorizationCode;
-
-	// @Schema(description = "상태", example = "")
-	// @NotBlank(message = "State must not be blank")
-	// private String state;
 
 	@Schema(description = "사용자 정보", example = "{\"name\":{\"firstName\":\"페인\",\"lastName\":\"카\"},\"email\":\"caffeineratrip@gmail.com\"}")
 	//@NotBlank(message = "User must not be blank")

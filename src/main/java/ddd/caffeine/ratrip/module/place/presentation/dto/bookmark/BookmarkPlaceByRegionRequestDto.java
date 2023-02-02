0ac550a0c03523cafc3ca4c9025dfc8008935df7
jihwan <sgh002400@naver.com>
2023-02-02 22:11:52 +0900

    fix(RequestDto): Dto 생성자 문제 해결

--- src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/bookmark/BookmarkPlaceByRegionRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/bookmark/BookmarkPlaceByRegionRequestDto.java
@@ -1,15 +1,22 @@
 package ddd.caffeine.ratrip.module.place.presentation.dto.bookmark;
 
+import javax.validation.constraints.NotNull;
+
 import ddd.caffeine.ratrip.module.place.application.dto.BookmarkPlaceByRegionDto;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Getter
 public class BookmarkPlaceByRegionRequestDto {
-	private double latitude;
+	@NotNull(message = "Latitude must not be null")
+	private final double latitude;
 
-	private double longitude;
+	@NotNull(message = "Longitude must not be null")
+	private final double longitude;
+
+	public BookmarkPlaceByRegionRequestDto(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 
 	public BookmarkPlaceByRegionDto toServiceDto() {
 		return BookmarkPlaceByRegionDto.of(latitude, longitude);

fa1f3edaad00f427436c78e9c15a67d5f474f80c
JIWEON-JEONG <dlektl6@naver.com>
2023-02-03 17:27:33 +0900

    remove : 안쓰는 메서드 삭제

--- src/main/java/ddd/caffeine/ratrip/common/validator/RequestDataValidator.java
+++ src/main/java/ddd/caffeine/ratrip/common/validator/RequestDataValidator.java
@@ -31,14 +31,6 @@ public class RequestDataValidator {
 		}
 	}
 
-	public static void validateIsNumber(String number) {
-		try {
-			Long.parseLong(number);
-		} catch (NumberFormatException e) {
-			throw new CommonException(INVALID_THIRD_PARTY_ID_EXCEPTION);
-		}
-	}
-
 	public static void validateUUIDForm(String uuid) {
 		final Pattern UUID_PATTERN = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
 		if (!(UUID_PATTERN.matcher(uuid).matches())) {

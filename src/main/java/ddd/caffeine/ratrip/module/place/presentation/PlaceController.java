8caebcdb735e288da21d23cc0af8f6cd1e8407de
jihwan <sgh002400@naver.com>
2023-02-03 15:47:43 +0900

    refactor(PlaceController): @Valid 어노테이션 삭제

--- src/main/java/ddd/caffeine/ratrip/module/place/presentation/PlaceController.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/presentation/PlaceController.java
@@ -136,8 +136,7 @@ public class PlaceController {
 	@Operation(summary = "[인증] 유저가 선택한 지역 기반 북마크 리스트 조회")
 	@GetMapping("/bookmarks/region")
 	public ResponseEntity<BookmarkPlacesByRegionResponseDto> getBookmarkPlacesByRegion(
-		@Parameter(hidden = true) @AuthenticationPrincipal User user,
-		@Valid @RequestParam Region region,
+		@Parameter(hidden = true) @AuthenticationPrincipal User user, @RequestParam Region region,
 		@PageableDefault(size = 20) Pageable pageable) {
 
 		return ResponseEntity.ok(placeService.getBookmarkPlacesByRegion(user, region, pageable));
